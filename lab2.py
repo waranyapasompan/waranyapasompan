0ff8e0a2c27b6e49d17f4f0449a69a36a68c45e6
amitaduangta25 <amitaduangta@gmail.com>
2021-12-14 10:04:07 +0700

    first commit

---
+++ lab2.py
@@ -0,0 +1,6 @@
+y = int(input("รับค่าแม่สูตร:"))
+i=1
+while i<=24 :
+    z=y*i
+    print(y,"x",i,"=",z)
+    i+=1
---
+++ lab4.py
@@ -0,0 +1,4 @@
+y=int(input("รับค่าแม่สูตร:"))
+for i in range(1,13,1):
+    z=y*i
+    print(y,"x",i,"=",z)
